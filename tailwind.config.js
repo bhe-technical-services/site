240d9e193c415210b1e46151bcab3e6d5123ee3e
GT Fari <guideg6@gmail.com>
2020-09-14 19:00:12 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// See https://tailwindcss.com/docs/configuration for details
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  experimental: {
+    applyComplexClasses: true,
+  },
+  purge: ["./src/**/*.js"],
+  theme: {},
+  variants: {},
+  // https://github.com/tailwindcss/custom-forms
+  plugins: [require("@tailwindcss/custom-forms")],
+};
