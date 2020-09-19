d76aad59dbc590804559048fb6799cf9332e659e
GT Fari <guideg6@gmail.com>
2020-09-20 00:10:57 +0200

    temporary footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,24 +2,24 @@ import React from 'react'
 
 function Footer() {
     return (
-        <footer className="bg-blue-700">
+        <footer className="bg-primary-green-600">
         <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
           <p className="text-white">
             Created by{` `}
             <a
               className="font-bold no-underline"
-              href="https://bryant.io"
+              href="https://goosebumps.co.zw"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Taylor Bryant
+              Goosebumps Collective
             </a>
           </p>
 
           <p>
             <a
               className="font-bold text-white no-underline"
-              href="https://github.com/taylorbryant/gatsby-starter-tailwind"
+              href="https://github.com/txndai/bts"
               target="_blank"
               rel="noopener noreferrer"
             >
