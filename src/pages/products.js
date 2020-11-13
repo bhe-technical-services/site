9850f9c7452d9ac1e616115cb389e556eb4dac04
GT Fari <guideg6@gmail.com>
2020-11-13 11:39:07 +0200

    featured product slider working.

--- src/pages/products.js
+++ src/pages/products.js
@@ -25,33 +25,38 @@ function Products() {
         <h2 className="mb-10 underline main-heading">Featured Products</h2>
         <div
           id="featuredProductSlider"
-          className="flex flex-row w-full overflow-x-scroll"
+          className="flex flex-no-wrap w-full overflow-x-auto scrolling-touch"
         >
+        {/* <div  className="flex px-4 mb-4"> */}
           {featuredProducts.products.map((product) => (
-            <div className="w-full p-4 lg:w-1/4 md:w-1/2" key={product.slug}>
+            <div className="w-full p-4 lg:w-1/4 md:w-1/2 card" key={product.slug}>
               <Link
                 to={`/products/${product.slug}`}
                 className="relative block h-48 overflow-hidden rounded"
               >
                 <Img
-                  className="block object-cover object-center w-full h-full"
+                  className="block object-cover object-center w-full h-full min-w-30"
                   fluid={product.image[0].node.childImageSharp.fluid}
                 />
               </Link>
               <div className="mt-4">
-                <h2 className="text-lg font-medium text-writing-black title-font">
+                <h2 className="text-lg font-bold text-writing-black title-font">
                   {product.name}
                 </h2>
               </div>
             </div>
           ))}
+          {/* </div> */}
         </div>
       </section>
       <section className="container px-5 py-24 mx-auto">
         <h2 className="mb-10 underline main-heading">Categories</h2>
         <div className="flex flex-wrap ">
           {productCategories.map((category) => (
-            <div className="w-full p-4 lg:w-1/4 md:w-1/2" key={category.slug}>
+            <div
+              className="w-full p-4 lg:w-1/4 md:w-1/2"
+              key={category.slug}
+            >
               <Link
                 to={`/products/categories/${category.slug}`}
                 className="relative block h-48 overflow-hidden rounded"
@@ -62,7 +67,7 @@ function Products() {
                 />
               </Link>
               <div className="mt-4">
-                <h2 className="text-lg font-medium text-writing-black title-font">
+                <h2 className="text-lg font-bold text-writing-black title-font">
                   {category.name}
                 </h2>
               </div>
