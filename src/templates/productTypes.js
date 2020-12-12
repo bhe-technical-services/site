b1d120b8032b90ebdccba918375b3d250643c1b0
GT Fari <guideg6@gmail.com>
2020-12-12 21:56:30 +0200

    update: new product UX as per client request

--- src/templates/productTypes.js
+++ src/templates/productTypes.js
@@ -8,52 +8,53 @@ import SEO from '../components/seo';
 export const pageQuery = graphql`
 query TypeQuery($slug: String) {
     gcms {
-        productType(where: { slug: $slug }) {
+      products: products(where: {productType_some: {slug_contains: $slug}}) {
         name
         slug
-        productCategories(where: {productType: {slug_contains: $slug}}) {
-          slug
-          featureImage {
-            url
-            node {
-              childImageSharp {
-                fluid(maxWidth: 560) {
+        image {
+          url
+          node {
+            childImageSharp {
+              fluid(maxWidth: 560) {
                   ...GatsbyImageSharpFluid
-                }
               }
             }
           }
-          name
         }
+      }
+      heading: productType(where: {slug: $slug}) {
+        name
       }
     }
   }
 
+
 `;
 
 
-const category = ( { data: {gcms: { productType }, }}) => {
+const category = ( { data: {gcms: { products, heading }, }}) => {
     // const { data: {gcms: { category }, }} = useStaticQuery(pageQuery);
-    console.log(productType)
-    const products = productType.productCategories
+    console.table(heading)
+    // const products = productType.productCategories
     return (
         <Layout>
-          <SEO
-            title={productType.name}
-            description={`All ${productType.name} in stock`}
+           <SEO
+            title={heading.name}
+            description={`All ${heading.name} products in stock`}
           />
+          
             <div className='container px-5 py-24 mx-auto'>
             <h2 className="mb-10 underline main-heading">
-                {productType.name}
+                {heading.name}
             </h2>
             <div className='flex flex-wrap '>
                     {products.map((product) => (
                         <div className="w-full p-4 lg:w-1/4 md:w-1/2" key={product.slug}>
-                            <Link to={`/products/categories/${product.slug}`} className="relative block h-48 overflow-hidden rounded">
-                                <Img className="block object-cover object-center w-full h-full" fluid={product.featureImage.node.childImageSharp.fluid} />
+                            <Link to={`/products/${product.slug}`} className="relative block h-48 overflow-hidden rounded">
+                                <Img className="block object-cover object-center w-full h-full" fluid={product.image[0].node.childImageSharp.fluid} />
                             </Link>
                                 <div className="mt-4">
-                                  <Link to={`/products/categories/${product.slug}`}>
+                                  <Link to={`/products/${product.slug}`}>
                                     <h2 className="text-lg font-bold text-writing-black title-font">{product.name}</h2>
                                   </Link>
                                 </div>
